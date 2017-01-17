a0d45f9c0bdaeb0c21d70bd570c264f201d7975e
Lnis <elenaht8@gmail.com>
2017-01-17 17:21:09 +0100

    Leer y crear ficheros

--- src/edu/femxa/val/exceptions/Array2Fichero.java
+++ src/edu/femxa/val/exceptions/Array2Fichero.java
@@ -12,33 +12,45 @@ public class Array2Fichero {
 	
 	public static String[] fromFichero2Array (File file) throws IOException
 	{
-		String[] lista_cadena = null;
+		
+		String[] lista_cadena = new String [4];
 		int i=0;
 		FileReader filereader=new FileReader (file);
 		BufferedReader br = new BufferedReader(filereader);
-		while (br.readLine() != null){
-			lista_cadena [i] = br.readLine();
+		while (br.ready()){
+			lista_cadena[i] = br.readLine();
+			System.out.println(lista_cadena[i]);
 			i++;
 		}
+		br.close();
+		filereader.close();
 		return lista_cadena; 
 	}
 	
 	
-	public static boolean fromArray2Fichero (String [] lista_cadena)
+	public static boolean fromArray2Fichero (String [] lista_cadena) throws IOException
 	{
 		boolean ok = false;
-		File file_dos=null;
+		File file2 = new File("C:\\Users\\Alumno\\git\\16DIC\\Carpetilla\\Envio\\Lineas2.txt");
 		int longi=lista_cadena.length;
-		FileWriter filewriter=new FileWriter (file_dos);
+		FileWriter filewriter=new FileWriter (file2);
 		BufferedWriter bw = new BufferedWriter(filewriter);
 		for (int i=0; i<longi; i++){
-			bw.write(String lista_cadena)
+			bw.write(lista_cadena[i]);
 			bw.newLine();
 		}
-		
+		bw.close();
+		filewriter.close();
 		return ok;
 		
 		
 	}
 
+	public static void main(String[] args) throws IOException {
+		File file = new File("C:\\Users\\Alumno\\git\\16DIC\\Carpetilla\\Envio\\Lineas.txt");
+		String[] lista= null;
+		lista=fromFichero2Array(file);
+		fromArray2Fichero(lista);
+		
+	}
 }
